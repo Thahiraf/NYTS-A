7e49a330c5de1cf19323f472dfe25dc354fb5c0c
Thahira <thahiraf@gmail.com>
2021-07-08 09:54:49 +0400

    Initial Commit

---
+++ NYTS/NYTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTSApp.swift
+//  NYTS
+//
+//  Created by Thahi on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NYTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
